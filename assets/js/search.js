a635b3debf2895c199cbc60562c27149a0085494
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-01-28 22:13:53 -0500

    enter key also searches

--- assets/js/search.js
+++ assets/js/search.js
@@ -12,22 +12,35 @@ function filterElements(searchVal) {
 
     }
   }  else {
-    for (let element of elements){
+    for (let element of elements) {
       element.style.display = "";
     }
   }
 }
+
+function updateUrl(){
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchVal = document.getElementById("search-bar").value;
+  urlParams.set('search', searchVal);
+  window.location.search = urlParams.toString();
+}
+
+document.getElementById("search-bar").addEventListener('keypress', function(event) {
+  if(event.keyCode === 13) {
+    updateUrl();
+  }
+});
+
 document.getElementById("search-bar").addEventListener('input', function(event) {
   var searchVal = document.getElementById("search-bar").value;
   filterElements(searchVal);
 });
+
 document.getElementById("search-button").addEventListener('click', function(event) {
-  console.log("hello world");
-  const urlParams = new URLSearchParams(window.location.search);
-  const searchVal = document.getElementById("search-bar").value;
-  urlParams.set('search',searchVal);
-  window.location.search = urlParams.toString();
+  updateUrl();
 });
+
+
 
 // On load
 const urlParams = new URLSearchParams(window.location.search);
